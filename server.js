05976912c808fc858f1c94cd4148ff9b9c84aa65
geekfly <wangxxx51@gmail.com>
2021-03-14 22:55:25 +0800

    v2

--- server.js
+++ server.js
@@ -24,9 +24,18 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`
+      <!DOCTYPE html>
+      <head>
+        <title>server</title>
+        <link rel="stylesheet" href="./style.css">
+      </head>
+      <body>
+        <h1>这是一个h1标签</h1>
+      </body>
+    `)
     response.end()
-  } else if(path === '/x'){
+  } else if(path === '/style.css'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color: red;}`)
